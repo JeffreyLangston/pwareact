977dfc71f61b5fa7e4e66d3a167cffa127fc919b
Jeff Langston <info@jefflangston.ca>
2022-03-30 08:39:58 -0600

    added video display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactMediaRecorder } from "react-media-recorder";
+import { useReactMediaRecorder } from "react-media-recorder";
 
 function App() {
+
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ video: true, askPermissionOnMount: true,blobPropertyBag: { type: "video/mp4" } });
+
+
+  var saveBlob = () => {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+  
+        var url = mediaBlobUrl;
+        a.href = url;
+        a.download = 'video.mp4';
+        a.click();
+        window.URL.revokeObjectURL(url);
+  
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Video Recorder</div>
+
+      <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+      <button onClick={saveBlob}>Download</button> 
+    </div>
+
     </div>
   );
 }
